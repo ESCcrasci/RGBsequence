0183bea460e84988d855a1bb634fb3a5335d52e2
ESCcrasci <32393938+ESCcrasci@users.noreply.github.com>
2017-09-29 20:09:45 +0530

    Add files via upload

---
+++ rgb_control.ino
@@ -0,0 +1,27 @@
+
+#define ledPinred 4
+#define ledPinBLUE 2
+#define ledPinGREEN 3
+void setup() {
+ pinMode(ledPinred,OUTPUT);  
+  pinMode(ledPinBLUE,OUTPUT);  
+   pinMode(ledPinGREEN,OUTPUT);  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(ledPinred, HIGH);
+  delay(1000);
+    digitalWrite(ledPinred, LOW);
+    delay (1000);
+
+  digitalWrite(ledPinBLUE, HIGH);
+  delay(1000);
+  digitalWrite(ledPinBLUE, LOW);
+    delay (1000);
+
+  digitalWrite(ledPinGREEN, HIGH);
+  delay(1000);
+  digitalWrite(ledPinGREEN, LOW);
+    delay (1000);
+}
